8cae9bdf0f372833fad1cede477995a69ef2669e
gatoora <mm.pourdad@gmail.com>
2024-02-19 01:20:19 +0330

    kamel shod

--- bingo.py
+++ bingo.py
@@ -1,7 +1,7 @@
 # bingo game functional
 import random
 randnum = random.randint(0, 10)
-guess_left = 4
+guess_left = 3
 user_name = input('Enter your name: ')
 
 
@@ -11,4 +11,18 @@ def check_answer(answer):
     if answer < randnum:
         return (False, "Choose higher number")
     if answer == randnum:
-        return (True, "Bingo ", user_name, "You did it")
+        return (True, "Bingo %s You did it" % user_name)
+
+
+while True:
+    if guess_left == 0:
+        print(user_name, " you ran out of guess..losser")
+        break
+    answer_input = int(input("enter your guess: "))
+    answer_result = check_answer(answer_input)
+    if answer_result[0] == False:
+        print(answer_result[1])
+    else:
+        print(answer_result[1])
+        break
+    guess_left -= 1
