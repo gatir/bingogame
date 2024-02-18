51ddc97848ef5f35866e1e5d7cc415a90cfe1ed0
gatoora <mm.pourdad@gmail.com>
2024-02-19 01:08:42 +0330

    function check answer done

--- bingo.py
+++ bingo.py
@@ -1,1 +1,14 @@
 # bingo game functional
+import random
+randnum = random.randint(0, 10)
+guess_left = 4
+user_name = input('Enter your name: ')
+
+
+def check_answer(answer):
+    if answer > randnum:
+        return (False, "choose lower number")
+    if answer < randnum:
+        return (False, "Choose higher number")
+    if answer == randnum:
+        return (True, "Bingo ", user_name, "You did it")
