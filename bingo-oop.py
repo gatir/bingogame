9f8dd21c63b14d315a4b1891a7c79a635708ac60
gatoora <mm.pourdad@gmail.com>
2024-02-19 03:22:09 +0330

    check answer added

--- bingo-oop.py
+++ bingo-oop.py
@@ -6,6 +6,15 @@ class BingoGame:
 
     def __init__(self):
         self.name = input("Enter your name: ")
-        self.rand_num = random.randint(0, 10)
-        self.guess_left = 3
+        self.__rand_num = random.randint(0, 10)
+        self.__guess_left = 3
+        self.__win_state = False
         self.player_list.append(self)
+
+    def check_answer(self, answer):
+        if answer > self.__rand_num:
+            return (False, "choose lower number")
+        if answer < self.__rand_num:
+            return (False, "Choose higher number")
+        if answer == self.__rand_num:
+            return (True, "Bingo %s You did it" % self.name)
